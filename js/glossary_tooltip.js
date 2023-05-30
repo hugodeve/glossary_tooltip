d93abcb3d50b62a5c2ec9474c4eb151dbe2e0b7a
Victor Ramirez <globalr7on@gmail.com>
2023-05-30 14:55:31 -0300

    Fix glossary_tooltip.module and glossary_tooltip. js

--- js/glossary_tooltip.js
+++ js/glossary_tooltip.js
@@ -1,15 +1,55 @@
+function sendMessageToBackgroundScript(message) {
+  return new Promise(function (resolve, reject) {
+    chrome.runtime.sendMessage(message, function (response) {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+}
+
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.glossaryTooltip = {
     attach: function (context) {
-      $(drupalSettings.glossary_tooltip.selector, context).each(function () {
-        var description = $(this).data('description');
-        $(this).tooltip({
-          content: description,
-          show: {
-            effect: 'slide',
-            duration: 300
-          }
-        });
+      var selector = drupalSettings.glossaryTooltip && drupalSettings.glossaryTooltip.selector ? drupalSettings.glossaryTooltip.selector : '.glossary-tooltip';
+
+      $(selector, context).each(function () {
+        var $term = $(this);
+
+        if (!$term.hasClass('glossary-tooltip-processed')) {
+          var $tooltip = $('<div class="glossary-tooltip-tooltip"></div>');
+          var description = drupalSettings.glossaryTooltip.terms[$term.text()];
+          $tooltip.text(description);
+          $term.hover(
+            function () {
+              $tooltip.appendTo('body');
+            },
+            function () {
+              $tooltip.remove();
+            }
+          );
+          $term.mousemove(function (e) {
+            $tooltip.css({
+              top: e.clientY + 10,
+              left: e.clientX + 10
+            });
+          });
+
+          $term.addClass('glossary-tooltip-processed');
+
+          // Send a message to the background script and wait for the response
+          sendMessageToBackgroundScript({ processedTerm: $term.text() })
+            .then(function (response) {
+              // Handle the response from the background script
+              console.log(response);
+            })
+            .catch(function (error) {
+              // Handle any errors that occurred
+              console.error(error);
+            });
+        }
       });
     }
   };
