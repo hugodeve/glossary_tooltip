988a22ec0162b4f384d08472f63bbf0c7030564b
Victor Ramirez <globalr7on@gmail.com>
2023-06-01 03:29:01 -0300

    Fit code

--- js/glossary_term.js
+++ js/glossary_term.js
@@ -1,48 +1,26 @@
-function sendMessageToBackgroundScript(message) {
-  return new Promise(function (resolve, reject) {
-    chrome.runtime.sendMessage(message, function (response) {
-      if (chrome.runtime.lastError) {
-        reject(chrome.runtime.lastError);
-      } else {
-        resolve(response);
-      }
-    });
-  });
-}
+(function (Drupal, drupalSettings) {
+  Drupal.behaviors.glossaryTerm = {
+    attach: function (context) {
+      // Get the glossary terms from drupalSettings.
+      let terms = drupalSettings.glossary_term && drupalSettings.glossary_term.terms ? drupalSettings.glossary_term.terms : {};
 
-(function ($) {
-  Drupal.behaviors.glossaryTermTooltips = {
-    attach: function (context, settings) {
-      console.log('here')
-      // Verificar si la propiedad 'terms' existe en el objeto de configuración de Drupal.
-      if (
-        typeof settings.glossary_term !== 'undefined' &&
-        typeof settings.glossary_term.terms !== 'undefined'
-      ) {
-        // Obtén los términos del glosario del objeto de configuración de Drupal.
-        let glossaryTerms = settings.glossary_term.terms;
-        console.log(glossaryTerms);
+      // Iterate through each glossary term in the content.
+      let glossaryTerms = document.querySelectorAll('.glossary-term');
+      glossaryTerms.forEach(function (term) {
+        let keyword = term.textContent;
+        let description = terms[keyword];
+        console.log(description);
 
-        // Agrega tooltips a los nodos que contienen palabras clave del glosario.
-        $(once('glossaryTooltip', '.glossary-highlight', context)).each(function () {
-          let term = $(this).text().trim();
-          
+        if (description) {
+          // Create the tooltip element with the description.
+          let tooltip = document.createElement('span');
+          tooltip.className = 'glossary-termtext';
+          tooltip.textContent = description;
 
-          // Verifica si el término está en el glosario.
-          if (term in glossaryTerms) {
-            let description = glossaryTerms[term];
-            let tooltipOptions = {
-              content: description,
-              position: { my: 'left top', at: 'left bottom', collision: 'flipfit' },
-              show: { effect: 'fade', duration: 300 },
-              hide: { effect: 'fade', duration: 300 },
-            };
-
-            // Agrega el tooltip utilizando jQuery UI Tooltip.
-            $(this).tooltip(tooltipOptions);
-          }
-        });
-      }
+          // Append the tooltip element after the keyword element.
+          term.parentNode.insertBefore(tooltip, term.nextSibling);
+        }
+      });
     }
   };
-})(jQuery);
+})(Drupal, drupalSettings);
